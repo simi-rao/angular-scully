ad9779c1745e1e4c00dbd4b7b3eb8e260ad3920d
Simi Rao <simisudarshan.rao@cat.com>
2021-09-14 14:15:17 +0530

    puppeteer changes

--- scully.angular-scully.config.ts
+++ scully.angular-scully.config.ts
@@ -3,6 +3,9 @@ export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "angular-scully",
   outDir: './dist/static',
+  puppeteerLaunchOptions: {
+    args: ["-no-sandbox", "-disable-setuid-sandbox"],
+  },
   routes: {
   }
 };
